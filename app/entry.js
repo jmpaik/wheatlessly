01064fada11ffafabcfbb732371c4ce32a378b4b
Bhavya Bharti <bhavyab@outlook.com>
2017-02-10 16:15:50 -0800

    log and router config

--- app/entry.js
+++ app/entry.js
@@ -11,23 +11,23 @@ const ngTouch = require('angular-touch');
 const ngAnimate = require('angular-animate');
 const ngFileUpload = require('ng-file-upload');
 
-const cfgram = angular.module('cfgram', [ngTouch, ngAnimate, uiRouter, ngFileUpload]);
+const wheatlessly = angular.module('wheatlessly', [ngTouch, ngAnimate, uiRouter, ngFileUpload]);
 
 let context = require.context('./config/', true, /\.js$/);
 context.keys().forEach( path => {
-  cfgram.config(context(path));
+  wheatlessly.config(context(path));
 });
 
 context = require.context('./view/', true, /\.js$/);
 context.keys().forEach( key => {
   let name = pascalcase(path.basename(key, '.js'));
   let module = context(key);
-  cfgram.controller(name, module);
+  wheatlessly.controller(name, module);
 });
 
 context = require.context('./service/', true, /\.js$/);
 context.keys().forEach( key => {
   let name = camelcase(path.basename(key, '.js'));
   let module = context(key);
-  cfgram.service(name, module);
+  wheatlessly.service(name, module);
 });
---
+++ app/view/landing/landing-controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('./_landing.scss');
+
+module.exports = ['$log', '$location', '$rootScope', 'authService', landingController];
+
+function landingController($log, $location){
+  $log.debug('landingController');
+
+	 let url = $location.url();
+	 this.showHome = url === '' || '/landing' || '/';
+}
