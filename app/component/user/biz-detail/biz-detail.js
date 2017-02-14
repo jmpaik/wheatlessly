9012486670c47a860bfcf990db1f5cd2dd565467
Bhavya Bharti <bhavyab@outlook.com>
2017-02-14 14:43:28 -0800

    biz detail page

---
+++ app/component/user/biz-detail/biz-detail.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('./_biz-detail.scss');
+
+module.exports = {
+  template: require('./biz-detail.html'),
+  controller: ['$log', 'authService', 'bizService', bizDetailController],
+  controllerAs: 'bizDetailCtrl'
+};
+
+function bizDetailController($log, authService, bizService){
+  $log.debug('bizDetailController');
+
+  this.biz = {};
+
+
+
+};
