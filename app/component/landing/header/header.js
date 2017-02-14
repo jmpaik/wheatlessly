d83b1b8d4a3404e8df94ce3190f9364ee8c050e3
Bhavya Bharti <bhavyab@outlook.com>
2017-02-14 15:49:53 -0800

    header error fix

--- app/component/landing/header/header.js
+++ app/component/landing/header/header.js
@@ -26,7 +26,7 @@ function headerController($log, $location, authService){
     authService.logout();
   };
   this.showLoginPage = function(){
-
+    
     $location.path('/home');
     this.showLogin = true;
     this.showSignup = false;
@@ -41,14 +41,10 @@ function headerController($log, $location, authService){
   this.landing = function(){
     this.showLogin = false;
     this.showSignup = false;
-
     $location.path('/');
+  };
 
-  }
-};
-
-  }
   this.business = function(){
     $location.path('/create-biz');
-  }
+  };
 };
