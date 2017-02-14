24e70a4666fc9a10e2bc9f27f1c16aeeb9d0d43c
Bhavya Bharti <bhavyab@outlook.com>
2017-02-14 10:48:04 -0800

    logout and login

--- app/component/landing/login/login.js
+++ app/component/landing/login/login.js
@@ -4,29 +4,27 @@ require('./_login.scss');
 
 module.exports = {
   template: require('./login.html'),
-  controller: ['$log', '$location', 'authService', LoginController],
+  controller: ['$log', '$location', '$window', 'authService', LoginController],
   controllerAs: 'loginCtrl'
-};
+}
 
-function LoginController($log, $location, authService) {
+function LoginController($log, $location, $window, authService) {
   $log.debug('LoginController');
 
-  this.user = {};
 
-  authService.getToken()
-  .then( () => {
-    $location.url('/home');
-  });
   this.login = function(user) {
     $log.log('loginCtrl.login()');
 
 
     authService.login(user)
     .then( () => {
-      $location.url('/home');
+      $location.path('/home');
+      $window.location.reload();
       $log.log('you are loggedin');
+      return;
     })
     .catch( err => {
+      alert('email or password is wrong!');
       $log.error('failed login:', err);
     });
   };
